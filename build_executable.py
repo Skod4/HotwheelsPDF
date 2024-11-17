8a3996b36e02014b1be55c43209c8b1e1ea6ae6d
Skod4 <ndelacruzdesousa@gmail.com>
2024-11-17 14:39:21 -0500

    Update GitHub workflow to properly handle releases and artifacts

--- build_executable.py
+++ build_executable.py
@@ -1,17 +1,20 @@
 import PyInstaller.__main__
 import os
-import sys
+import platform
 
 def build():
     # Get the directory containing images
     images_dir = os.path.join('hotwheelspdf', 'images')
+    
+    # Platform-specific path separator
+    path_sep = ';' if platform.system() == 'Windows' else ':'
     
     PyInstaller.__main__.run([
         'run.py',
         '--name=HotwheelsPDF',
         '--onefile',
         '--windowed',
-        f'--add-data={images_dir}{os.pathsep}images',
+        f'--add-data={images_dir}{path_sep}images',
         '--icon=hotwheelspdf/images/LogoBackground01.png',
         '--clean',
     ])
